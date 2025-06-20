36e3adf0dbf4363b592503d5d22cf4f6d8aa40e8
crapple12 <enoestephon@gmail.com>
2025-06-20 16:25:02 -0400

    Update library.js

--- library.js
+++ library.js
@@ -13,19 +13,49 @@ function Book (title, author, pages, status) {
   this.id = crypto.randomUUID();
 }
 
+function render() {
+    let libraryEl = document.querySelector(".browse");
+    libraryEl.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookEl = document.createElement("div");
+        bookEl.innerHTML = `
+        <div class="card">
+            <div class="card-header">
+                <h3 class="title">${book.title}<h3>
+                <h5 class="author">${book.author}<h5>
+            </div>
+            <div class="card-body">
+                <p>${book.pages} pages</p>
+                <p class="status">${book.status}<p>
+            </div>
+        </div>
+        `;
+        libraryEl.appendChild(bookEl);
+    }
+}
+
 function addBookToLibrary() {
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let pages = document.querySelector("#pages").value;
     let status = document.querySelector("#status").value;
     let addBook = new Book(title, author, pages, status);
-    console.log(addBook);
+    myLibrary.push(addBook);
+    render();
 }
 
 let addBookbtn = document.querySelector("#add-book-btn");
+let closeForm = document.querySelector(".close");
+let addBookForm = document.querySelector("#add-book-form");
 addBookbtn.addEventListener("click", function() {
-    let addBookForm = document.querySelector("#add-book-form");
     addBookForm.style.display = "flex";
+    closeForm.style.display = "block";
+})
+
+closeForm.addEventListener("click", function() {
+    addBookForm.style.display = "none";
+    closeForm.style.display = "none";
 })
 
 document.querySelector("#add-book-form").addEventListener("submit", function(event) {
@@ -33,3 +63,4 @@ document.querySelector("#add-book-form").addEventListener("submit", function(eve
     addBookToLibrary();
 })
 
+// url(https://png.pngtree.com/background/20210711/original/pngtree-classical-ink-painting-bamboo-background-material-picture-image_1130331.jpg)
